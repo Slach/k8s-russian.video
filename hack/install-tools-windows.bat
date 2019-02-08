df57733403a7686371e381b0c399367f4ac309bf
Slach <bloodjazman@gmail.com>
2019-02-08 06:08:17 +0300

    update install tools for windows
    Signed-off-by: Slach <bloodjazman@gmail.com>

--- hack/install-tools-windows.bat
+++ hack/install-tools-windows.bat
@@ -1,2 +1,6 @@
+# please run this script wiht Administrator Right (WIN+X)
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install virtualbox
+choco install vagrant
+choco install docker-toolbox
 choco install kubernetes-cli minikube
